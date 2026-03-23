0bfb075f0189197b9c3084d3a47f4a593bde8e3c
Martin Palmetzhofer <martin.palme04@gmail.com>
2026-03-23 18:56:08 +0100

    Fix wrong data types in init silver layer

--- Scripts/Silver/init_silver_layer.sql
+++ Scripts/Silver/init_silver_layer.sql
@@ -17,13 +17,14 @@ IF OBJECT_ID('silver.crm_prd_info', 'U') IS NOT NULL
 	DROP TABLE silver.crm_prd_info;
 CREATE TABLE silver.crm_prd_info (
 	prd_id INT,
+	cat_id NVARCHAR(50),
 	prd_key NVARCHAR(50),
 	prd_nm NVARCHAR(50),
 	prd_cost INT,
 	prd_line NVARCHAR(50),
-	prd_start_dt DATETIME,
-	prd_end_dt DATETIME,
-    dwh_load_date DATETIME2 DEFAULT GETDATE()
+	prd_start_dt DATE,
+	prd_end_dt DATE,
+    dwh_load_dt DATETIME2 DEFAULT GETDATE()
 );
 
 IF OBJECT_ID('silver.crm_sales_details', 'U') IS NOT NULL
@@ -32,9 +33,9 @@ CREATE TABLE silver.crm_sales_details (
 	sls_ord_num NVARCHAR(50),
 	sls_prd_key NVARCHAR(50),
 	sls_cust_id INT,
-	sls_order_dt INT,
-	sls_ship_dt INT,
-	sls_due_dt INT,
+	sls_order_dt DATE,
+	sls_ship_dt DATE,
+	sls_due_dt DATE,
 	sls_sales INT,
 	sls_quantity INT,
 	sls_price INT,
@@ -47,26 +48,26 @@ CREATE TABLE silver.crm_sales_details (
 IF OBJECT_ID('silver.erp_cust_az12', 'U') IS NOT NULL
 	DROP TABLE silver.erp_cust_az12;
 CREATE TABLE silver.erp_cust_az12 (
-	CID NVARCHAR(50),
-	BDATE DATE,
-	GEN NVARCHAR(50),
+	cid NVARCHAR(50),
+	bdate DATE,
+	gen NVARCHAR(50),
     dwh_load_date DATETIME2 DEFAULT GETDATE()
 );
 
 IF OBJECT_ID('silver.erp_loc_a101', 'U') IS NOT NULL
 	DROP TABLE silver.erp_loc_a101;
 CREATE TABLE silver.erp_loc_a101 (
-	CID NVARCHAR(50),
-	CNTRY NVARCHAR(50),
+	cid NVARCHAR(50),
+	cntry NVARCHAR(50),
     dwh_load_date DATETIME2 DEFAULT GETDATE()
 );
 
 IF OBJECT_ID('silver.erp_px_cat_g1v2', 'U') IS NOT NULL
 	DROP TABLE silver.erp_px_cat_g1v2;
 CREATE TABLE silver.erp_px_cat_g1v2 (
-	ID NVARCHAR(50),
-	CAT NVARCHAR(50),
-	SUBCAT NVARCHAR(50),
-	MAINTENANCE NVARCHAR(50),
+	id NVARCHAR(50),
+	cat NVARCHAR(50),
+	subcat NVARCHAR(50),
+	maintenance NVARCHAR(50),
     dwh_load_date DATETIME2 DEFAULT GETDATE()
 );
